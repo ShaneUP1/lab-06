c727d29fddb5440582c836a6ade700c7c615d87a
Shane Upchurch <shaneupchurch@gmail.com>
2020-11-02 16:16:27 -0600

    port fix

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.json([
+        {
+            name: 'spot'
+        },
+        {
+            name: 'rover'
+        }
+    ]);
+});
+
+app.get('/drinks', (req, res) => {
+    const drinks = [
+        {
+            name: 'apple juice'
+        },
+        {
+            name: 'beer'
+        },
+        {
+            name: 'sprite'
+        }
+    ];
+
+    res.json(drinks);
+});
+
+app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`);
+});
